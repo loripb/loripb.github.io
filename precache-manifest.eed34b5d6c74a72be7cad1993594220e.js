de7573b1dea1a1a847d4465694029772f7eef1d9
Lori Boyd <lori.p.boyd@gmail.com>
2020-05-13 12:26:15 -0400

    Updates

---
+++ precache-manifest.eed34b5d6c74a72be7cad1993594220e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9835c86690f620d8026cbc4cb44ff23c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a178377d76130a762d",
+    "url": "/static/css/main.8677c5c5.chunk.css"
+  },
+  {
+    "revision": "8e86d9377778479e1d63",
+    "url": "/static/js/2.60e868dd.chunk.js"
+  },
+  {
+    "revision": "59eee5a20441efe8cf9652b2cc007836",
+    "url": "/static/js/2.60e868dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a178377d76130a762d",
+    "url": "/static/js/main.ecda5bd1.chunk.js"
+  },
+  {
+    "revision": "5f0b7b663cf3b6fd040c",
+    "url": "/static/js/runtime-main.a160e612.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
